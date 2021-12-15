85fa7775aafd9654711e81ed7d68999110200e64
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-12-15 12:12:31 +0100

    Add user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userShema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("User", userShema);
