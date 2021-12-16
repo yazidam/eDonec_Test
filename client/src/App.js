df8c4221f6576e74b9726be1612d462bb5fbc95f
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-12-16 14:58:04 +0100

    adding footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center my-3 ">
+            eDonec by Ahmed Yazid Mejri 😏
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
