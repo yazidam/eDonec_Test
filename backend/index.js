5ed7a9c27a10b740804c757ff0ad15f2e8f03b47
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-12-16 10:18:42 +0100

    Adding route product

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
 
 dotenv.config();
 connectDB();
 app.use(express.json());
 
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`serveur runnig in port ${PORT} 😆`));
 // console.log("test ");
---
+++ backend/routes/productRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { createProduct } = require("../controllers/productController");
+
+router.route("/").post(createProduct);
+
+module.exports = router;
