4b00f28c9787ae0031c613fea0526d2b24a8f9f1
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-12-16 15:53:00 +0100

    adding product reducer

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import "./bootstrap.min.css";
-const reducer = combineReducers({});
+import { productListReducer } from "./reducers/productReducers";
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
 const initialState = {};
 
