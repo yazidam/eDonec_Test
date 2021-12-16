c4f5a67188fdeedffd38ba9e9d28d0451b07b26e
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-12-16 21:23:26 +0100

    adding product screen

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
+        <Route path="/product/:id" element={<ProductScreen />} />
       </Routes>
       <Footer />
     </BrowserRouter>
