6ffe4e900469bf7fa199767c144c986ddf18882d
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-12-16 16:27:36 +0100

    showing product list

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
---
+++ client/src/screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
+import { Row, Col } from "react-bootstrap";
+
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Lates products</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Row>
+          {products?.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
