56f282880aea17c9f833e74835f9db4c44692fc8
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-12-17 11:59:43 +0100

    adding cart reducer

--- client/src/store.js
+++ client/src/store.js
@@ -6,9 +6,11 @@ import {
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
 const initialState = {};
