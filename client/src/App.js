3762624044500762910c7df931929415c28bce29
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-12-18 13:00:57 +0100

    fixing login screen

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,23 @@ import React from "react";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
-import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <main className="mx-3">
-        <Container>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:id" element={<ProductScreen />} />
-            <Route path="/cart/:id/*" element={<CartScreen />} />
-            <Route path="/login" element={<LoginScreen />} />
-          </Routes>
-        </Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/product/:id" element={<ProductScreen />} />
+          <Route path="/cart/:id/*" element={<CartScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/shipping" element={<ShippingScreen />} />
+        </Routes>
       </main>
       <Footer />
     </BrowserRouter>
--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,7 +1,104 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import { login } from "../actions/userActions";
+import image from "../phone.jpg";
 const LoginScreen = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let location = useLocation();
+  let navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+  console.log("object", redirect);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(`/${redirect}`);
+    }
+  }, [userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+  return (
+    <Row>
+      <Col
+        md={6}
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "95vh",
+        }}
+      ></Col>
+
+      <Col md={6} style={{ width: "100vh" }}>
+        <h1 style={{ textAlign: "center" }}> Sign In</h1>
+        <i
+          className="fas fa-lock"
+          style={{
+            fontSize: "35px",
+            color: "#406882 ",
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "5px",
+          }}
+        ></i>
+        {error && <h2 className="text-center">CHECK YOUR COORDINATE </h2>}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="email" className="mx-5">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password" className="mx-5">
+            <Form.Label>Password </Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button
+            type="submit"
+            variant="primary"
+            className="my-3 "
+            disabled={email.length < 10}
+            className="mx-5 my-5"
+            style={{ width: "60vh", borderRadius: "5px" }}
+          >
+            Sign In
+          </Button>
+
+          <Row className="text-center">
+            <Col>
+              New Coustomer?{" "}
+              <Link
+                to={redirect ? `/register?redirect=${redirect}` : "/register"}
+              >
+                Register
+              </Link>
+            </Col>
+          </Row>
+        </Form>
+      </Col>
+    </Row>
+  );
 };
 
 export default LoginScreen;
