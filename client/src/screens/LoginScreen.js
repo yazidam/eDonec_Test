547941a0fb8a0cbd4616ce957276fd51293a94c7
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-12-18 20:13:49 +0100

    fix

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -14,7 +14,7 @@ const LoginScreen = () => {
   const dispatch = useDispatch();
   const userLogin = useSelector((state) => state.userLogin);
   const { loading, error, userInfo } = userLogin;
-  const redirect = location.search ? location.search.split("=")[1] : "/";
+  const redirect = location.search ? location.search.split("=")[1] : "";
   console.log("object", redirect);
 
   useEffect(() => {
